9c4ec0df75c80ce5ac68663604dd8f7c856c30fd
d070426 <patrick.guenther01@sap.com>
2022-04-28 16:11:01 +0200

    Update loss_func.py

--- func/loss_func.py
+++ func/loss_func.py
@@ -153,8 +153,13 @@ class WeightedCrossEntropyLoss(nn.Module):
         self.ignore_index = ignore_index
 
     def forward(self, input, target):
-        weight = self._class_weights(input)
-        return F.cross_entropy(input, target, weight=weight, ignore_index=self.ignore_index)
+        # reshape from (batch, 1, 64, 64, 64) to (batch, 64, 64, 64)
+        inp = torch.reshape(input, (input.shape[0],
+                                    input.shape[2],
+                                    input.shape[3],
+                                    input.shape[4]))
+        weight = self._class_weights(inp)
+        return F.cross_entropy(inp, target, weight=weight, ignore_index=self.ignore_index)
 
     @staticmethod
     def _class_weights(input):
