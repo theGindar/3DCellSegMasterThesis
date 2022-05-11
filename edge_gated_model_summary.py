4f64130416cac769e2f1227a392f9fed33b9aa44
d070426 <patrick.guenther01@sap.com>
2022-05-11 16:20:08 +0200

    new model

--- edge_gated_model_summary.py
+++ edge_gated_model_summary.py
@@ -1,4 +1,4 @@
-from func.network import CellSegNet_basic_edge_gated, CellSegNet_basic_lite
+from func.network import CellSegNet_basic_edge_gated, CellSegNet_basic_lite, CellSegNet_basic_edge_gated_II
 from torchinfo import summary
 
 model=CellSegNet_basic_lite(input_channel=1, n_classes=3, output_func = "softmax")
@@ -6,3 +6,7 @@ print(summary(model, (1, 1, 64, 64, 64)))
 
 model=CellSegNet_basic_edge_gated(input_channel=1, n_classes=3, output_func = "softmax")
 print(summary(model, (1, 1, 64, 64, 64)))
+
+model=CellSegNet_basic_edge_gated_II(input_channel=1, n_classes=3, output_func = "softmax")
+print(summary(model, (1, 1, 64, 64, 64)))
+
