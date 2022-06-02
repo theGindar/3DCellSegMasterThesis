ef172d6171744d0416cc60ace7952e39f037ec9d
d070426 <patrick.guenther01@sap.com>
2022-06-02 16:23:21 +0200

    Update train_HMS_w_groupnorm.py

--- train_HMS_w_groupnorm.py
+++ train_HMS_w_groupnorm.py
@@ -24,12 +24,11 @@ boundary_importance = 1
 batch_size = 7
 num_workers = 4
 # ----------
-# get current device
-print(f"current device used: {torch.cuda.device_count()}")
 # init model
 model=CellSegNet_basic_lite_w_groupnorm(input_channel=1, n_classes=3, output_func = "softmax")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+# get current device
+print(f"current device used: {torch.cuda.device_count()}")
 model.to(device)
 
 if need_resume and os.path.exists(load_path):
