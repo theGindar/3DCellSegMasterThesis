e1237b3bd98b83c36b66ca824ae7bc59450fb49a
d070426 <patrick.guenther01@sap.com>
2022-06-08 17:05:06 +0200

    edge gated second try

--- edge_gated_model_summary.py
+++ edge_gated_model_summary.py
@@ -1,4 +1,4 @@
-from func.network import CellSegNet_basic_edge_gated, CellSegNet_basic_lite, CellSegNet_basic_lite_w_groupnorm_deep_supervised_XI
+from func.network import CellSegNet_basic_edge_gated_IX, CellSegNet_basic_lite, CellSegNet_basic_lite_w_groupnorm_deep_supervised_XI
 from torchinfo import summary
 
 model=CellSegNet_basic_lite(input_channel=1, n_classes=3, output_func = "softmax")
@@ -8,6 +8,6 @@ print(summary(model, (1, 1, 64, 64, 64)))
 # print(summary(model, (1, 1, 64, 64, 64)))
 
 print("NEW")
-model=CellSegNet_basic_lite_w_groupnorm_deep_supervised_XI(input_channel=1, n_classes=3, output_func = "softmax")
+model=CellSegNet_basic_edge_gated_IX(input_channel=1, n_classes=3, output_func = "softmax")
 print(summary(model, (1, 1, 64, 64, 64)))
 
