faa6a061c9ce109fb878677e911d726758c1cf5c
d070426 <patrick.guenther01@sap.com>
2022-06-11 15:00:30 +0200

    test stuff

--- train_HMS_w_groupnorm_new_loss.py
+++ train_HMS_w_groupnorm_new_loss.py
@@ -114,5 +114,5 @@ for ith_epoch in range(0, max_epoch):
     if (ith_epoch+1)%model_save_freq==0:
         print('epoch: '+str(ith_epoch+1)+' save model')
         model.to(torch.device('cpu'))
-        torch.save({'model_state_dict': model.state_dict()}, f'model_HMS_w_groupnorm_new_loss.pkl')
+        torch.save({'model_state_dict': model.state_dict()}, f'output/model_HMS_w_groupnorm_new_loss.pkl')
         model.to(device)
