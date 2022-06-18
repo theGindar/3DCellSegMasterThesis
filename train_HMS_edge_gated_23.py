eee96aff2e9e847b1637d5a4328c0ae7cd1e687b
d070426 <patrick.guenther01@sap.com>
2022-06-18 20:59:24 +0200

    Update train_HMS_edge_gated_23.py

--- train_HMS_edge_gated_23.py
+++ train_HMS_edge_gated_23.py
@@ -139,7 +139,7 @@ for ith_epoch in range(0, max_epoch):
 
         weights_boundary = (batch['weights_boundary'].to(device) + weights_consistency).to(device)
         # print(f"weights_consistency shape: {weights_consistency.shape}")
-        print(f"weights_background shape: {batch['weights_background'].shape}")
+        # print(f"weights_background shape: {batch['weights_background'].shape}")
         # print(f"weights_boundary shape: {weights_boundary.shape}")
         weights_background = batch['weights_background'].to(device)
         weights_bb = torch.cat((weights_background, weights_boundary), dim=1).to(device)
