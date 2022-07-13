f6c770cd783258bc61d056787fade35b522ac6e9
d070426 <patrick.guenther01@sap.com>
2022-07-13 17:23:32 +0200

    Update graph_learning_new_training_loop_focal_loss_Ovules_retrained.py

--- graph_learning_new_training_loop_focal_loss_Ovules_retrained.py
+++ graph_learning_new_training_loop_focal_loss_Ovules_retrained.py
@@ -131,6 +131,7 @@ for e in range(300):
         # del sample_graph
         # sample_graph.clear()
         # sample_graph.detach().cpu()
+        sample_graph.ndata['feat'] = torch.tensor([])
         torch.cuda.empty_cache()
         print('{:.1f} MiB'.format(torch.cuda.max_memory_allocated() / 1000000))
 
