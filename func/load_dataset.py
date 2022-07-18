7451ad3b501882ff6c3f69693fa187053685ae4a
d070426 <patrick.guenther01@sap.com>
2022-07-18 17:02:56 +0200

    Update load_dataset.py

--- func/load_dataset.py
+++ func/load_dataset.py
@@ -473,7 +473,7 @@ class Cell_Seg_3D_Dataset_limit_background(Dataset):
             raw_3d_img = np.array(hf["raw"])
             seg_boundary = np.array(hf["boundary"])
             seg_foreground = np.array(hf["foreground"])
-            seg_edge = np.array(hf["edge"])
+            seg_edge = np.array(hf["edge_boundary"])
             seg_edge_foreground = np.array(hf["edge_foreground"])
 
             seg_edge_background = np.array(hf["edge_background"])
