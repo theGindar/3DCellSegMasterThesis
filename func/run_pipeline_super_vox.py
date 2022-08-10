6130e0a02fc9ceb672d6b0c2246e4673b614d58b
d070426 <patrick.guenther01@sap.com>
2022-08-10 16:31:49 +0200

    Update run_pipeline_super_vox.py

--- func/run_pipeline_super_vox.py
+++ func/run_pipeline_super_vox.py
@@ -347,7 +347,7 @@ def segment_super_vox_3_channel_gasp(raw_img, model, device,
     seg_final = seg_final.astype(np.int8)
 
     # make sure all elements are positive
-    compressed_pickle("../../../mnt2/debug_HMS_boundary.pbz2")
+    compressed_pickle("../../../mnt2/debug_HMS_boundary.pbz2", seg_foreground_comp)
 
     print(np.min(seg_final))
     seg_final = seg_final + 500
