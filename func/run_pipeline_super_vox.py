81d44a9ca78fa64967a44d3961d4eb156bb939ca
d070426 <patrick.guenther01@sap.com>
2022-08-10 15:41:25 +0200

    Update run_pipeline_super_vox.py

--- func/run_pipeline_super_vox.py
+++ func/run_pipeline_super_vox.py
@@ -339,6 +339,9 @@ def segment_super_vox_3_channel_gasp(raw_img, model, device,
     # make sure all elements are positive
     seg_final = seg_final + np.abs(np.min(seg_final)) + 1
 
+    print("DEBUG")
+    print(np.min(seg_final))
+
     return seg_final.astype(np.int8)
 
 
